51535dfb6bad1398b19fd056e813c967eaf2d7aa
Yedil <yedilmyrzabek@gmail.com>
2024-10-18 15:51:18 +0500

    Nariman Zhenis Yedil PART

--- Assignment - 4/main/java/org/example/StrategyPattern/main.java
+++ Assignment - 4/main/java/org/example/StrategyPattern/main.java
@@ -1,17 +1,37 @@
 package org.example.StrategyPattern;
 
+import java.util.Scanner;
+import java.util.concurrent.Flow;
+
 public class main {
     public static void main(String[] args) {
-        User user = new User(new FreeSub());
-        user.accessContent();
 
-        user.setSubscription(new PremiumSub());
-        user.accessContent();
 
-        user.setSubscription(new FamilySub());
-        user.accessContent();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select your Subscription: " +
+                "\n1. Free Subscription" +
+                "\n2. Premium Subscription" +
+                "\n3. Family Subscription");
 
-        user.setSubscription(new FamilySub());
-        user.accessContent();
+        int answer = sc.nextInt();
+        switch (answer){
+            case 1:
+                User user = new User(new FreeSub());
+                user.accessContent();
+                break;
+            case 2:
+                User user1 = new User(new PremiumSub());
+                user1.accessContent();
+                break;
+            case 3:
+                User user2 = new User(new FamilySub());
+                user2.accessContent();
+                break;
+            default:
+                System.out.println("Sorry this number is wrong");
+        }
+
+
+
     }
 }
