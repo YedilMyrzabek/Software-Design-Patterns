26e7e5db68840d6be8c192eba1ae40b88c3bc780
esimb <230723@astanait.edu.kz>
2024-11-05 13:29:36 +0300

    add opportunity for executing payment methods (Context)

---
+++ FinalProject/main/java/org/example/Strategy (Payments Methods)/PaymentContext.java
@@ -0,0 +1,11 @@
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public PaymentContext(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
